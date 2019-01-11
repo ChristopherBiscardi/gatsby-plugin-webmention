959fd5b848fe6022acd89445c894707c9170d374
ChristopherBiscardi <chris@christopherbiscardi.com>
2019-01-11 13:10:52 -0800

    no jsx in wraproot

--- wrap-root-element.js
+++ wrap-root-element.js
@@ -3,40 +3,50 @@ import { Helment } from "react-helmet";
 
 export const wrapRootElement = (
   { element },
-  { username, pingbacks, webmentions, forwardPingbacksAsWebmentions, identity }
-) => (
-  <Fragment>
-    <Helmet>
-      {webmentions &&
-        username && (
-          <link
-            rel="webmention"
-            href={`https://webmention.io/${username}/webmention`}
-          />
-        )}
-      {pingbacks &&
-        username && (
-          <link
-            rel="pingback"
-            href={`https://webmention.io/${username}/xmlrpc`}
-          />
-        )}
-      {forwardPingbacksAsWebmentions &&
-        username && (
-          <link
-            rel="pingback"
-            href={`https://webmention.io/webmention?forward=${
-              forwardPingbacksAsWebmentions
-            }`}
-          />
-        )}
-      {identity.twitter && (
-        <link href={`https://twitter.com/${identity.twitter}`} rel="me" />
-      )}
-      {identity.github && (
-        <link href={`https://github.com/${identify.github}`} rel="me" />
-      )}
-    </Helmet>
-    {element}
-  </Fragment>
-);
+  {
+    username,
+    pingbacks = false,
+    webmentions = true,
+    forwardPingbacksAsWebmentions = false,
+    identity
+  }
+) =>
+  React.createElement(
+    Fragment,
+    null,
+    React.createElement(
+      Helmet,
+      null,
+      webmentions &&
+        username &&
+        React.createElement("link", {
+          rel: "webmention",
+          href: `https://webmention.io/${username}/webmention`
+        }),
+      pingbacks &&
+        username &&
+        React.createElement("link", {
+          rel: "pingback",
+          href: `https://webmention.io/${username}/xmlrpc`
+        }),
+      forwardPingbacksAsWebmentions &&
+        username &&
+        React.createElement("link", {
+          rel: "pingback",
+          href: `https://webmention.io/webmention?forward=${
+            forwardPingbacksAsWebmentions
+          }`
+        }),
+      identity.twitter &&
+        React.createElement("link", {
+          href: `https://twitter.com/${identity.twitter}`,
+          rel: "me"
+        }),
+      identity.github &&
+        React.createElement("link", {
+          href: `https://github.com/${identify.github}`,
+          rel: "me"
+        })
+    ),
+    element
+  );
